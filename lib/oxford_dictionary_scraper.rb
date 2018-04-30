e2d3a0b7762289cc1dbb9c873f96ac0020943dda
TrinhPhong <nguyenquoctin921996@gmail.com>
2018-04-30 23:47:50 +0700

    Change way to get data

--- lib/oxford_dictionary_scraper.rb
+++ lib/oxford_dictionary_scraper.rb
@@ -2,28 +2,34 @@ class OxfordDictionaryScraper
   def initialize; end
 
   def scraping
-    words = read_file
+    first_chars = ['A-B', 'C-D', 'E-G', 'H-K', 'L-N', 'O-P', 'Q-R', 'S', 'T', 'U-Z']
     result = []
-    words.each_with_index do |word, idx|
-      sleep 10 if idx % 100 == 0
+    first_chars.each do |first_char|
+      puts '~~~~~~~~~~~~~~~~~~~~~~~~~'
+      puts "Start scraping #{first_char}"
+      doc = Nokogiri::HTML(open('https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_' + first_char))
+      page = 1
+      while doc.search('.paging_links li').last.values.first != 'activepage'
+        doc = Nokogiri::HTML(open('https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_' + first_char + '/?page=' + page.to_s))
+        entry_list = doc.search('#entrylist1 .result-list1 li a')
+        entry_list.each do |entry|
+          link = entry.values.first
+          word_doc = Nokogiri::HTML(open(link))
 
-      doc = Nokogiri::HTML(open('https://www.oxfordlearnersdictionaries.com/definition/english/' + word))
-
-      word_class                  = get_word_class(doc)
-      sn_gs_li                    = get_sn_gs_li(doc)
-      definition_include_examples = get_definition_include_examples(sn_gs_li)
-
-      result << { word: word, word_class: word_class, sense: definition_include_examples }
+          word_class                  = get_word_class(word_doc)
+          sn_gs_li                    = get_sn_gs_li(word_doc)
+          definition_include_examples = get_definition_include_examples(sn_gs_li)
+          result << { word: entry.text, word_class: word_class, sense: definition_include_examples }
+        end
+        puts 'Complete Page ' + page.to_s
+        page += 1
+      end
+      puts "Complete #{first_char}!!!"
     end
-
     result
   end
 
   private
-
-  def read_file
-    File.read('lib/scraper/source_3k_words.txt').split("\n")
-  end
 
   def get_word_class(doc)
     doc.search('.webtop-g .pos').text

12466c88239d9bc55f94b989bc0b82896cd2772e
Phong Phan <fongfan999@gmail.com>
2018-05-14 15:19:25 +0700

    Set whenever log path

--- config/mina/whenever.rb
+++ config/mina/whenever.rb
@@ -1,11 +1,12 @@
 set :whenever_name, -> { "#{fetch(:application_name)}_#{fetch(:rails_env)}" }
+set :whenever_output_path, -> { "#{fetch(:shared_path)}/log/cron.log" }
 
 namespace :whenever do
   desc 'Update crontab'
   task update: :remote_environment do
     comment "Update crontab for #{fetch(:application_name)}"
     in_path fetch(:current_path) do
-      command "#{fetch(:bundle_prefix)} whenever --update-crontab #{fetch(:whenever_name)}"
+      command "#{fetch(:bundle_prefix)} whenever --update-crontab #{fetch(:whenever_name)} --set 'output=#{fetch(:whenever_output_path)}'"
     end
   end
 end

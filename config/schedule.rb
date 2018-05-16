1cd4b38085ba7032c26d5ac10b755d7cc6efcbba
Phong Phan <fongfan999@gmail.com>
2018-05-16 12:35:51 +0700

    Fix crontab UTC

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,7 @@ every 1.day, at: '0am' do
 end
 
 # Eg: Run from 7:45AM to 7:45PM, every 1 hour when the time difference is 15
-every "#{60 - TIME_DIFFERENCE_OF_VOCAB_DEVLIERY} 7-20/1 * * *" do
+every "#{60 - TIME_DIFFERENCE_OF_VOCAB_DEVLIERY} 0-13/1 * * *" do
   rake 'vocabulary:deliver'
 end
 

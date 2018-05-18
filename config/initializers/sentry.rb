11e3540aafcf80a7d74fb1be455f9339f0bb7b55
Phong Phan <fongfan999@gmail.com>
2018-05-18 23:40:51 +0700

    Set output for Sentry Logger

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,5 @@ Raven.configure do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.async = ->(event) { SentryJob.perform_later(event.to_hash) }
+  config.logger = Raven::Logger.new(STDOUT)
 end

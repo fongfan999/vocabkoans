d4bc1d02b7450a3459998ed679b81604699271ac
Phong Phan <fongfan999@gmail.com>
2018-05-18 23:30:14 +0700

    Integrate Sentry monitoring service to the app

---
+++ app/controllers/concerns/sentry_raven.rb
@@ -0,0 +1,13 @@
+module SentryRaven
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_raven_context
+  end
+
+  private
+
+  def set_raven_context
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+end

70b1ac6ebe73933090dd150a826ed33a3e32715f
Phong Phan <fongfan999@gmail.com>
2018-10-07 19:11:00 +0700

    Update server address

--- config/mina/environments/production.rb
+++ config/mina/environments/production.rb
@@ -2,7 +2,7 @@ task :production do
   set :rails_env, 'production'
 
   set :user, 'ubuntu'
-  set :domain, '13.229.229.72'
+  set :domain, '13.228.245.158'
   set :identity_file, '~/.ssh/ec2-default.pem'
 
   set :deploy_to, '/var/www/vocabkoans'
